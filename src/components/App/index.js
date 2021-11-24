1836fc16325288deb6fef99ed2ec964bf624827e
Corxis <jennblngr@gmail.com>
2021-11-25 00:16:03 +0100

    creation of the components and styles folder; installation of the destyle.css and sass dependencies

---
+++ src/components/App/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './styles.scss';
+
+const App = () => {
+  return (
+    <div className="app">
+      Develop your test here
+    </div>
+  );
+};
+
+export default App;
